1fb9883fccf3173812e8cec89d6a75ed37c549c5
liwenwne <1004749546@qq.com>
2018-06-14 21:34:59 +0800

    注册完成

--- js/zhuce.js
+++ js/zhuce.js
@@ -54,4 +54,64 @@ $(function(){
     	$(".li_top:parent").css({"background":"#333333","color":"white"})
     })*/
  })
- 
+ //注册方式切换
+ /*$("#btn_email").on("click",function(){
+ 	$(".change1").css("display","none");
+ 	$(".change2").css("display","block");
+ })
+ $("#btn_phone").on("click",function(){
+ 	$(".change1").css("display","block");
+ 	$(".change2").css("display","none");
+ })*/
+ $(function(){
+ 	 $("#btn_email").click(function(){
+    $(".change1").css("display","none");
+ 	$(".change2").css("display","block");
+ 	$(this).css({
+ 		"background": "#b00a2a",
+			"border":"1px solid #a90024",
+			"color": "white"});
+    $("#btn_phone").css({
+ 		"background": "#d9d7d7",
+			"border":"1px solid #cccccc",
+			"color": "black"});
+ 	});
+ 	$("#btn_phone").click(function(){
+    $(".change1").css("display","block");
+ 	$(".change2").css("display","none");
+ 	$(this).css({
+ 		"background": "#b00a2a",
+			"border":"1px solid #a90024",
+			"color": "white"});
+    $("#btn_email").css({
+ 		"background": "#d9d7d7",
+			"border":"1px solid #cccccc",
+			"color": "black"});
+ 	});
+
+ 	
+ })
+ //注册正则验证
+ //正则封装
+ function checkAll(type,value){
+	var reg=null;
+	switch(type){
+		case "username":reg=/^[a-zA-Z_]\w{5,15}$/;break;
+		case "email":reg=/^\w+@[a-zA-Z0-9]+\.(com|cn|net)$/;break;
+		case "phoneNumber": reg=/^1[^0126][0-9]{9}$/;break;
+		case "personId": reg=/^[1-9]\d{5}(19|20)\d{2}(0[1-9]|1[012])([012][1-9]|3[01])\d{3}[0-9xX]$/;break;
+		case "date":reg=/^(19|20)\d{2}[\.\-\/](0[1-9]|1[012])[\.\-\/]([012][1-9]|3[01])$/;break;
+		case "ipCheck":reg=/^((\d|\d{2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}(\d|\d{2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/;break;
+		case "chinese":reg=/^[\u4e00-\u9fa5]+$/;break;
+		default:;
+	}
+	if(reg!=null){
+		if(reg.test(value)){
+			return true;
+		}
+	}
+	return false;
+}
+ $(function(){
+   $("#phone").blur()
+ })
